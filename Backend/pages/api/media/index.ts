9806c1d7d06ee4c75789b7465bf5645372fe41e8
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-06 16:12:06 +0800

    refactor: remove console.logs

--- Backend/pages/api/media/index.ts
+++ Backend/pages/api/media/index.ts
@@ -6,18 +6,11 @@ import {getS3} from "../../../utils/getS3";
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
     console.log('Request received')
     try {
-        console.log('Getting bucket')
-
         const s3 = getS3();
         const buckets = await s3.listBuckets();
-        console.log('Buckets', buckets)
         const imagesBucket = await s3.getOrCreateBucket(currentBucket);
-        console.log('Getting objects')
         const images = await imagesBucket.getAllObjects();
-        console.log('Converting to JSON')
         const objects = await Promise.all(images.map(image => image.toJSON()))
-        console.log('Objects', JSON.stringify(objects))
-        console.log('Sending response')
         if (!Array.isArray(objects)) {
             res.status(500).json({statusCode: 500, message: "Cannot find image data"});
         }
