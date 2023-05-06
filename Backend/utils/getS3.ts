d845870c9e0caaef6c7f55b5193a3997d46f58c6
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-06 16:13:00 +0800

    feat: implemented hash function

--- Backend/utils/getS3.ts
+++ Backend/utils/getS3.ts
@@ -1,4 +1,5 @@
 import {S3Lib} from "s3-simplified";
+import {createHash} from 'crypto';
 
 export function getS3() {
     return new S3Lib({
@@ -7,5 +8,18 @@ export function getS3() {
             secret: process.env.secretAccessKey,
         },
         region: "ap-southeast-1",
-    })
+        objectCreation: {
+            hash: {
+                function: Hash,
+            }
+        },
+    });
+}
+
+async function Hash(buffer: Buffer, metadata: Record<string, string>): Promise<string> {
+    console.log("using hash function");
+    const metaDataStr = JSON.stringify(metadata, Object.keys(metadata).sort());
+    const hash = createHash('sha256').update(buffer).update(metaDataStr).digest('hex');
+    console.log(hash);
+    return hash;
 }
